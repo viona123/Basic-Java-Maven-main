c27b2edf2a6f6106ec9a8e49d6ddae5af26701d5
viona123 <syahputriviona@gmail.com>
2026-06-17 00:20:53 +0700

    fix

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -20,7 +20,7 @@ public class CounterTest {
 	}
 
 	@Test
-	public void testIncrement() {
+	public void testIncrement() { 
 		Counter testCounter = new Counter();
 		
 		for(int i = 1; i <10; i++) {
